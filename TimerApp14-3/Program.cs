3c282e8b026117388c738c2418924365e7cf15d1
Melissa DoddRuesing <melissa.doddruesing@gmail.com>
2018-10-12 12:07:59 -0500

    Add project files.

---
+++ TimerApp14-3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TimerApp14_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
